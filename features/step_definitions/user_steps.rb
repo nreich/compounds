50cfade28f45246a42b2806326e79e0cca4ca30f
Nick Reich <nreich@gmail.com>
2013-01-12 16:32:21 -0800

    Added cucumber tests for signing in (tests pass)

--- features/step_definitions/user_steps.rb
+++ features/step_definitions/user_steps.rb
@@ -5,6 +5,13 @@ def create_visitor
     password: "password", password_confirmation: "password" }
 end
 
+def create_user
+  create_visitor
+  delete_user
+  @user = FactoryGirl.create(:user, email: @visitor[:email],
+                                    password: @visitor[:password])
+end
+
 def find_user
   @user ||= User.first conditions: {email: @visitor[:email]}
 end
@@ -25,9 +32,25 @@ def sign_up
   find_user
 end
 
+def sign_in
+  visit '/users/sign_in'
+  fill_in "Email", with: @visitor[:email]
+  fill_in "user_password", with: @visitor[:password]
+  click_button "Sign in"
+end
+
 ### Given ###
 Given /^I am not logged in$/ do
   visit '/users/sign_out'
+end
+
+Given /^I do not exist as a user$/ do
+  create_visitor
+  delete_user
+end
+
+Given /^I exist as a user$/ do
+  create_user
 end
 
 ### When ###
@@ -73,6 +96,28 @@ When /^I sign up with a mismatched password confirmation$/ do
   sign_up
 end
 
+When /^I sign in with valid credentials$/ do
+  create_visitor
+  sign_in
+end
+
+When /^I return to the site$/ do
+  visit '/'
+end
+
+When /^I sign in with a wrong email$/ do
+  create_visitor
+  @visitor = @visitor.merge(email: "wrong_email@example.com")
+  sign_in
+end
+
+When /^I sign in with a wrong password$/ do
+  create_visitor
+  @visitor = @visitor.merge(password: "badpassword")
+  sign_in
+end
+
+
 ### Then ###
 Then /^I should see a successful sign up message$/ do
   page.should have_content "Welcome! You have signed up successfully."
@@ -100,4 +145,24 @@ end
 
 Then /^I should see a mismatched password message$/ do
   page.should have_content "Password doesn't match confirmation"
+end
+
+Then /^I see an invalid login message$/ do
+  page.should have_content "Invalid email or password."
+end
+
+Then /^I should be signed out$/ do
+  page.should have_content "Sign up"
+  page.should have_content "Login"
+  page.should_not have_content "Logout"
+end
+
+Then /^I see a successful sign in message$/ do
+  page.should have_content "Signed in successfully"
+end
+
+Then /^I should be signed in$/ do
+  page.should have_content "Logout"
+  page.should_not have_content "Sign up"
+  page.should_not have_content "Login"
 end
