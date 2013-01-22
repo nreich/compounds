08e0266aaa659f47f230f49d10badb3ae68e6716
Nick Reich <nreich@gmail.com>
2013-01-21 21:28:02 -0800

    Added show and index functions to User controller

---
+++ features/user_show.feature
@@ -0,0 +1,6 @@
+Feature: Show User
+  In order to learn more about a user
+  As a user
+  I want to to bel able to view a user's page
+
+  Scenario: 
--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -3,17 +3,41 @@ require 'spec_helper'
 describe UsersController do
 
   describe "GET 'index'" do
-    it "returns http success" do
-      get 'index'
-      response.should be_success
+    before :each do
+      @users = []
+      3.times do
+        user = FactoryGirl.create(:user)
+        @users << user
+      end
+    end
+
+    it "should return the :index view" do
+      get :index
+      response.should render_template :index
+    end
+
+    it "should populate an array of contacts" do
+      get :index
+      assigns(:users).should eq(@users)
     end
   end
 
   describe "GET 'show'" do
-    it "returns http success" do
-      get 'show'
-      response.should be_success
+    
+    before :each do
+      @user = FactoryGirl.create(:user)
     end
+    
+    it "should return the :show view" do
+      get :show, id: @user
+      response.should render_template :show
+    end
+    
+    it "should assign the requested user to @user" do
+      get :show, id: @user
+      assigns(:user).should eq(@user)
+    end
+
   end
 
 end
