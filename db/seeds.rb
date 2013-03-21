a804897a1b02ce53e5e8497a08deef66d45f4e25
Nick Reich <nreich@gmail.com>
2013-03-20 22:05:32 -0700

    Added basic data set to seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,33 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+User.delete_all
+10.times do |n|
+  User.create!(name: "user#{n}", email: "user#{n}@example.com",
+               password: "password", password_confirmation: "password")
+end
+
+Molecule.delete_all
+10.times do |n|
+  Molecule.create!(name: "molecule #{n}", molecular_weight: 120 + 10 * n)
+end
+
+Salt.delete_all
+10.times do |n|
+  Salt.create!(name: "salt #{n}")
+end
+
+Batch.delete_all
+10.times do |n|
+  n.times do
+    Batch.create!(lot_number: n, date: DateTime.now, initial_amount: 10 * n,
+                  barcode: "1234#{n}", salt_id: n, formula_weight: 150 + 10 * n,
+                  molecule_id: n)
+  end
+end
+
+Transaction.delete_all
+User.all.each do |user|
+  Transaction.create!(user_id: user.id, batch_id: user.id, amount: 2)
+end
+
